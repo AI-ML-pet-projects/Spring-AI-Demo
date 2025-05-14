1df0ddda0740d5396de2928b757d702d175f23fb
Quentin <quentindevops@gmail.com>
2025-05-14 21:38:11 +0700

    First commit

--- src/main/java/com/spring/ai/demo/ChatService.java
+++ src/main/java/com/spring/ai/demo/ChatService.java
@@ -1,4 +1,20 @@
 package com.spring.ai.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ChatService {
+
+    @Autowired
+    private ChatModel chatModel;
+
+    public String getResponse(String prompt) {
+        return chatModel.call(prompt);
+    }
+
 }
--- src/main/java/com/spring/ai/demo/GenAIController.java
+++ src/main/java/com/spring/ai/demo/GenAIController.java
@@ -1,4 +1,18 @@
 package com.spring.ai.demo;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GenAIController {
+    @Autowired
+    private ChatService chatService;
+
+    @GetMapping("ask")
+    public String ask(@RequestParam String prompt) {
+        return chatService.getResponse(prompt);
+    }
 }
